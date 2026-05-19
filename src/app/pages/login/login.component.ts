5149d94eb53a288f37c04c093e934025eac3fd9b
Seu Nome <guilhermesnts.profissional@gmail.com>
2026-05-19 11:00:15 -0300

    start

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  senha:string = '';
+  nome:string = '';
+  erro:boolean = false;
+}
