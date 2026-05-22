7e0132b2186af3363192bb6a0db551082d40e38e
Seu Nome <guilhermesnts.profissional@gmail.com>
2026-05-22 11:42:27 -0300

    register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
         pathMatch: 'full'
     }
 ];
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  nome:string = '';
+  email:string = '';
+  senha:string = '';
+  confirmarsenha:string = '';
+  endereco:string = '';
+  erro:boolean = false;
+  ischecked:boolean = false;
 }
