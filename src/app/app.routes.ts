0bd889f3ae7811e357bc6e469f8889f896dc51c7
Seu Nome <guilhermesnts.profissional@gmail.com>
2026-05-26 11:32:21 -0300

    home done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
     {
         path: 'login',
         component: LoginComponent,
